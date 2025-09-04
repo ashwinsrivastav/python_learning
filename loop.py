bd3bbae7703a3c260c88b9ee7d3506fd21d64150
ashwinsrivastav <bashusrivastavmrj@gmail.com>
2025-09-04 23:13:41 +0530

    new

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
 a=int(input('enter a number'))
 upper_space=a
 lower_space=1
-for i in range(0,a+1):
+for i in range(a+1):
     for b in range(i):
         print('*',end='')
     for b in range(upper_space,0,-1):
